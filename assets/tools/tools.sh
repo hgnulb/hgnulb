85700b46a7e2c67b301d7822b6650abd9ec57afd
libin <lblibin@didiglobal.com>
2021-03-12 14:31:45 +0800

    no message

--- assets/tools/tools.sh
+++ assets/tools/tools.sh
@@ -3,12 +3,14 @@ echo "Please enter the file name"
 read -r name
 today=$(date "+%F-")
 time=$(date "+%Y-%m-%d %H:%M:%S")
+num=$(openssl rand -base64 8|cksum|cut -c 1-8)
+echo $num
 (
   cat <<EOF
 ---
 layout: post
 title: "$name"
-permalink: /blog/
+permalink: /blog/$num
 categories: []
 tags: []
 date: $time
